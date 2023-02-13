c7c372fd10b81ac6946222c7cd608cfbe389239f
Andrew Lusk <andrewlu@amazon.com>
2023-02-12 19:09:17 -0800

    Fixes to build w/o intervention

--- package/uqm/uqm.mk
+++ package/uqm/uqm.mk
@@ -9,4 +9,13 @@ UQM_SOURCE = uqm-$(UQM_VERSION)-src.tgz
 UQM_SITE = https://downloads.sourceforge.net/project/sc2/UQM/0.8
 UQM_DEPENDENCIES = host-pkgconf sdl libpng libvorbis libzip
 
+define FIX_PERMS
+    chmod +x output/build/uqm-$(UQM_VERSION)/configure
+    touch output/build/uqm-$(UQM_VERSION)/NEWS
+endef
+
+define UQM_POST_PATCH_HOOKS
+    FIX_PERMS
+endef
+
 $(eval $(autotools-package))
